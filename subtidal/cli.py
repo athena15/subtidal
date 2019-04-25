2db36291b9bf7cb6d896fef65a031217cfe05f05
Brenner Heintz <brenner.heintz@gmail.com>
2019-04-24 20:43:52 -0700

    Merge branch 'master' of /Users/brenner/PycharmProjects/subtidal with conflicts.

--- subtidal/cli.py
+++ subtidal/cli.py
@@ -9,6 +9,7 @@ from subtidal.download import download
 @click.argument('directory', default=os.getcwd(), required=True)
 # @click.option('--min-size-mb', '-s', type=click.INT,
 #               help='Minimum size (in MB) that video files must be for subtitles to be downloaded.')
+@click.option('--language', '-l', help=')
 @click.option('--verbose', '-v', is_flag=True, help='Prints more output to the console.')
 def download_subtitles(directory, verbose=False):
     download(directory, verbose)
